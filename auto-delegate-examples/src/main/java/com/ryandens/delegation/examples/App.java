bf792339043f8a4bade6882a3890e6fb387553ce
Ryan Dens <ryandens735@gmail.com>
2024-02-16 11:24:35 -0800

    :art:

--- auto-delegate-examples/src/main/java/com/ryandens/delegation/examples/App.java
+++ auto-delegate-examples/src/main/java/com/ryandens/delegation/examples/App.java
@@ -9,20 +9,21 @@ import java.util.logging.Logger;
 
 public final class App {
 
-    public static void main(final String[] args) throws SQLException {
-        try (Connection connection = DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")) {
-            Logger.getLogger(App.class.getName()).info("" + new App().doStuff(connection));
-        }
+  public static void main(final String[] args) throws SQLException {
+    try (Connection connection =
+        DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1")) {
+      Logger.getLogger(App.class.getName()).info("" + new App().doStuff(connection));
     }
+  }
 
-    private int doStuff(final Connection connection) throws SQLException {
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("CALL IDENTITY();");
-        if (!resultSet.next()) {
-            throw new SQLException("couldn't find any identity record");
-        }
-        int id = resultSet.getInt(1);
-        resultSet.close();
-        return id;
+  private int doStuff(final Connection connection) throws SQLException {
+    Statement statement = connection.createStatement();
+    ResultSet resultSet = statement.executeQuery("CALL IDENTITY();");
+    if (!resultSet.next()) {
+      throw new SQLException("couldn't find any identity record");
     }
+    int id = resultSet.getInt(1);
+    resultSet.close();
+    return id;
+  }
 }
