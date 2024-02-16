7d92584d36d371c9fe40764afc2bc68146cb091d
Ryan Dens <ryandens735@gmail.com>
2024-02-16 10:07:47 -0800

    :alembic: test pr improvement

--- auto-delegate-examples/src/main/java/com/ryandens/delegation/examples/PartialDelegationExample.java
+++ auto-delegate-examples/src/main/java/com/ryandens/delegation/examples/PartialDelegationExample.java
@@ -1,6 +1,7 @@
 package com.ryandens.delegation.examples;
 
 import com.ryandens.delegation.AutoDelegate;
+import java.util.logging.Logger;
 
 /** Base interface. */
 interface Base {
@@ -18,13 +19,18 @@ interface Extension extends Base {
  */
 @AutoDelegate(Base.class)
 class ExtensionImpl extends AutoDelegate_ExtensionImpl implements Extension {
+  private final int myIntegerValue;
 
   ExtensionImpl(final Base delegate) {
     super(delegate);
+    myIntegerValue = Integer.valueOf("3").intValue();
   }
 
   @Override
   public String bar() {
+    logger.info(String.format("Processing AutoDelegate annotations %d", myIntegerValue));
     return "bar";
   }
+
+  private static final Logger logger = Logger.getLogger(ExtensionImpl.class.getName());
 }
